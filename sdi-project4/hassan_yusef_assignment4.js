90fc7c76174a896e052f9b9ef8c7859793e82167
Yusef Hassan <Bioassassin@Yusefs-MacBook-Pro.local>
2012-03-21 11:55:56 -0400

    added url function

--- sdi-project4/hassan_yusef_assignment4.js
+++ sdi-project4/hassan_yusef_assignment4.js
@@ -11,12 +11,19 @@ var frame = {
 	email: function (e) {
 		var ePattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 			return ePattern.test(e)
- 	}	
+ 	},
+ 	
+ 	internet: function (u) {
+ 		var iPattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.+=&%@!\-\/]))?/;
+ 			return iPattern.test(u)
+ 	}
+	
+	
 	
 };
 console.log(frame.number(3304128538))
-console.log(frame.email("cardean13@yahoo.com"))
-
+console.log(frame.email("cardean13@fullsail.edu"))
+console.log(frame.internet("http://www.androidguys.com/"))
 
 
 
