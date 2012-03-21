dfda97fab5701b8bf79ddb4d5d0a8831566b4bdd
Yusef Hassan <Bioassassin@Yusefs-MacBook-Pro.local>
2012-03-21 09:29:26 -0400

    added phone number function

--- sdi-project4/hassan_yusef_assignment4.js
+++ sdi-project4/hassan_yusef_assignment4.js
@@ -1,1 +1,30 @@
 alert("JavaScript works!");
+
+
+var frame = {
+	
+	number: function (p) {
+		var pattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+			return pattern.test(p)
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+};
+console.log(frame.number(3304128538));
+
+
+
+
+
+
+
+
+
+
+
