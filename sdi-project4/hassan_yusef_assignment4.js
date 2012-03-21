dbc494de2e4b7b99fb92f51e1944e3e6a8903845
Yusef Hassan <Bioassassin@Yusefs-MacBook-Pro.local>
2012-03-21 10:01:39 -0400

    added email function

--- sdi-project4/hassan_yusef_assignment4.js
+++ sdi-project4/hassan_yusef_assignment4.js
@@ -4,22 +4,18 @@ alert("JavaScript works!");
 var frame = {
 	
 	number: function (p) {
-		var pattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-			return pattern.test(p)
-	}
+		var pPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+			return pPattern.test(p)
+	},
 	
-	
-	
-	
-	
-	
-	
+	email: function (e) {
+		var ePattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+			return ePattern.test(e)
+ 	}	
 	
 };
-console.log(frame.number(3304128538));
-
-
-
+console.log(frame.number(3304128538))
+console.log(frame.email("cardean13@yahoo.com"))
 
 
 
