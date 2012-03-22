863d3148d59df5e46edc4d1f90e9e2e4e7a204ae
Yusef Hassan <Bioassassin@sundns8.sssnet.com>
2012-03-22 10:49:41 -0400

    added string to number function

--- sdi-project4/hassan_yusef_assignment4.js
+++ sdi-project4/hassan_yusef_assignment4.js
@@ -8,20 +8,27 @@ var frame = {
 			return pPattern.test(p)
 	},
 	
+	
 	email: function (e) {
 		var ePattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 			return ePattern.test(e)
  	},
+ 	
  	
  	internet: function (u) {
  		var iPattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.+=&%@!\-\/]))?/;
  			return iPattern.test(u)
  	},
  	
+	
 	separate: function (a,b,c) {
 		var sPattern = a.split(b)
 			return sPattern.join (c)
-	
+	},
+    
+    
+    stringNumber: function (s) {
+    	return parseInt (s)
     }
  
   
@@ -31,7 +38,7 @@ console.log(frame.number(3304128538))
 console.log(frame.email("cardean13@fullsail.edu"))
 console.log(frame.internet("http://www.androidguys.com/"))
 console.log(frame.separate("xnnnynnnznnn", "nnn", "1"))
-
+console.log(frame.stringNumber("123456789"))
 
 
 
